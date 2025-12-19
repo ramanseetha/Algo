42b2ff1555e134bc877f83730517b9494d1c9aae
getnos <ramanseetah1208@gmail.com>
2025-12-19 16:31:49 +0530

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -110,20 +110,21 @@ export default function RootLayout({
 
       <body className={`${montserrat.className} antialiased`}>
         {/* ===== Meta Pixel Code ===== */}
+        {/* ===== Meta Pixel Code ===== */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}
-            (window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2252849898562075');
-            fbq('track', 'PageView');
-          `}
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}
+    (window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1188692866663469');
+    fbq('track', 'PageView');
+  `}
         </Script>
 
         <noscript>
@@ -131,9 +132,11 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2252849898562075&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1188692866663469&ev=PageView&noscript=1"
           />
         </noscript>
+        {/* ===== End Meta Pixel Code ===== */}
+
         {/* ===== End Meta Pixel Code ===== */}
 
         {children}
